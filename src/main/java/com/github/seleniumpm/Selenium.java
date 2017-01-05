d1299dada5b0a57a3e33ce45b9bab7eeb9a1c69b
Peter Salas <psalas@gmail.com>
2017-01-05 14:07:27 -0800

    Adding Apache License to all classes

--- src/main/java/com/github/seleniumpm/Selenium.java
+++ src/main/java/com/github/seleniumpm/Selenium.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Peter Salas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.seleniumpm;
 
 import org.openqa.selenium.WebDriver;
@@ -6,6 +22,10 @@ import org.openqa.selenium.WebElement;
 import java.net.URISyntaxException;
 import java.util.List;
 
+/**
+ * Interface to normalize between Selenium 1.0 and 2.0 (WebDriver). Ultimately, this makes it simple to do migration
+ * between the original Selenium and using WebDriver.
+ */
 public interface Selenium extends com.thoughtworks.selenium.Selenium {
     public WebDriver getDriver();
 
--- src/main/java/com/github/seleniumpm/SeleniumWebdriver.java
+++ src/main/java/com/github/seleniumpm/SeleniumWebdriver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Peter Salas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.seleniumpm;
 
 import org.apache.commons.lang.NotImplementedException;
@@ -12,6 +28,9 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Implementation of the Selenium interface using WebDriver
+ */
 public class SeleniumWebdriver extends RemoteWebDriver implements Selenium {
     protected WebDriver driver;
     protected URI baseURI;
